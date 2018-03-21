6441b5ac477af18c76868dc93761981a990acab7
Samer Kanjo <skanjo@12digitmarketing.com>
2018-03-21 13:43:25 -0500

    ref #7 Reduced some maven config, started integration test work.

---
+++ http/src/test/java/io/xorshift/hips/HipsHttpIT.java
@@ -0,0 +1,14 @@
+package io.xorshift.hips;
+
+import org.junit.Test;
+
+/**
+ * @author Samer Kanjo
+ * @since 0.6.0 3/21/18 12:19 AM
+ */
+public class HipsHttpIT {
+  @Test
+  public void name() {
+    System.out.println("INTEGRATION TEST");
+  }
+}
